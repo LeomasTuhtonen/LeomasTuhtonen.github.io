8f63c03911ea670e72ab4c20cd610aaf47bdeac8
LeomasTuhtonen <tuomas.lehtonen@gmail.com>
2025-07-20 13:01:38 +0300

    Add frame model save/load feature and fix clearing

--- test/integration.js
+++ test/integration.js
@@ -5,5 +5,7 @@ const html = fs.readFileSync('index.html', 'utf8');
 
 assert(/id=["']addSpanBtn["']/.test(html), 'Add Span button not found');
 assert(/id=["']exportBtn["']/.test(html), 'Export button not found');
+assert(/id=["']frameSaveBtn["']/.test(html), 'Frame Save button not found');
+assert(/id=["']frameLoadBtn["']/.test(html), 'Frame Load button not found');
 
 console.log('Integration tests passed');
